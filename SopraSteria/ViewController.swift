222f885aa77115166f558865e6829c22fb8708eb
Supertramp <umwelt.hugo@gmail.com>
2018-05-15 09:31:39 +0200

    Initial Commit

---
+++ SopraSteria/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SopraSteria
+//
+//  Created by BMGH SRL on 15/05/2018.
+//  Copyright © 2018 BMAGH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
