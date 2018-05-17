ab69dba33cf119786d8fa4a814d5534039eed80e
Supertramp <umwelt.hugo@gmail.com>
2018-05-17 10:22:38 +0200

    LatestViewController

---
+++ SopraSteria/Controller/LatestViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LatestViewController.swift
+//  SopraSteria
+//
+//  Created by BMGH SRL on 17/05/2018.
+//  Copyright © 2018 BMAGH. All rights reserved.
+//
+
+import UIKit
+
+class LatestViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+}
